e9d354f561003c10e9a1a5d229a3fbe85c1a3e93
prabath sanjeewa <prabathslakmal@gmail.com>
2023-12-03 12:28:21 +0530

    Basic file setup

--- cms-frontend/src/app/app.module.ts
+++ cms-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/view/layouts/header/header.component';
+import { FooterComponent } from './view/layouts/footer/footer.component';
+import { HomeComponent } from './view/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
